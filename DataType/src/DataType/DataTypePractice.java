70053e9fca25a15b444b4053c0b8ba81c90a3f76
Saurabh Nimje <saurabhnimje3@gmail.com>
2026-07-24 18:02:46 +0530

    Data Types Projects

---
+++ DataType/src/DataType/DataTypePractice.java
@@ -0,0 +1,36 @@
+package DataType;
+
+public class DataTypePractice {
+	byte b = 127; // Range of byte is -128 to 127
+	
+	short s = 246; 
+	
+	int  i = '/';
+	
+	long l = 29301039093090342l;
+	
+	char c = 40;
+	
+	float f = 094088.0987644323424f;
+	
+	double d = 3785783.8364963868638563;
+	
+	boolean flag = true;
+	
+	String str = "GDJah8e908e87whchuh";
+	
+	public static void main(String[] args) {
+		
+		DataTypePractice DT = new DataTypePractice();
+		System.out.println(DT.c);
+		System.out.println(DT.str);
+		System.out.println(DT.l);
+		System.out.println(DT.s);
+		System.out.println(DT.b);
+		System.out.println(DT.i);
+		System.out.println(DT.f);
+		System.out.println(DT.d);
+		System.out.println(DT.flag);
+		
+	}
+}
